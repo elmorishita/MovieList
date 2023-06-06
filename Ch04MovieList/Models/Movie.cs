0c0556017a241e1053d71fe032e42d61dcb2f1e1
Elijah Morishita <elijah.lm@gmail.com>
2023-06-05 20:59:40 -0500

    Add project files.

---
+++ Ch04MovieList/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ch04MovieList.Models
+{
+    public class Movie
+    {
+        // EF Core will configure the db to generate this value
+        public int MovieId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1889, 2999, ErrorMessage = "Year must be after 1889.")]
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a rating.")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int? Rating { get; set; }
+    }
+}
